0f018de1f5cffb768b98a818ed8417fa3087208d
ammobear <chocobo32>
2013-10-15 22:39:31 -0700

    Update with image

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
   console.log('We are live!');
+  updateBoardImage()
 });
+
+function updateBoardImage(){
+    $.ajax({ url: "/images", success: 
+        function(data, textStatus, jqXHR){
+            $("#board-image").attr("src","http://bing.com/" + data.images[0].url);
+        } 
+    });
+}
