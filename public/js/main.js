fb45045c41d7131ed0b4394e61e1962b476ea87b
ammobear <chocobo32>
2013-10-17 09:18:12 -0700

    Board has background image rather than image tag. Added CSS file

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,9 @@ $(document).ready(function(){
 function updateBoardImage(){
     $.ajax({ url: "/images", success: 
         function(data, textStatus, jqXHR){
-            $("#board-image").attr("src","http://bing.com/" + data.images[0].url);
+
+            $("#board-image").css("background-image", "url(http://bing.com/" + data.images[0].url + ")");
+            $("#board-image").css("background-repeat", "no-repeat");
         } 
     });
 }
