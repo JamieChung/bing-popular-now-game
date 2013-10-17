c99cc9984770dadaca8b55a810d75a5f8e3419d2
ammobear <chocobo32>
2013-10-17 14:15:04 -0700

    Shuffle images and text

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ app.configure(function(){
 
 // Homepage
 app.get('/', function (req, res) {
-    
     res.render('index', createPiecesArray());
 });
 
@@ -197,6 +196,20 @@ function getJS() {
     };
 }
 
+//http://www.kirupa.com/html5/shuffling_array_js.htm
+function shuffle(input) {
+     
+    for (var i = input.length-1; i >=0; i--) {
+     
+        var randomIndex = Math.floor(Math.random()*(i+1)); 
+        var itemAtIndex = input[randomIndex]; 
+         
+        input[randomIndex] = input[i]; 
+        input[i] = itemAtIndex;
+    }
+    return input;
+}
+
 function createPiecesArray()
 {
     subjects = getJS().popular_search_trends;
@@ -207,6 +220,7 @@ function createPiecesArray()
       cloned.isImage = true;  
       subjectsGo.push(cloned);
     }
+    subjectsGo = shuffle(subjectsGo);
     return { "popular_search_trends":subjectsGo };
 }
 
